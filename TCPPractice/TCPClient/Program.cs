9fc8a83378b1a1ad068522070ec5b2f3f9104454
Артур <kornet130902@gmail.com>
2021-04-08 20:26:35 +0300

    +Client TCP

--- TCPPractice/TCPClient/Program.cs
+++ TCPPractice/TCPClient/Program.cs
@@ -1,13 +1,61 @@
 ﻿using System;
+using System.IO;
+using System.Media;
+using System.Net.Sockets;
+using System.Text;
 
 namespace TCPClient
 {
     class Program
     {
+        const int port = 8888;
+        const string address = "127.0.0.1";
         static void Main(string[] args)
         {
-            const int port = 8888;
-            const string address = "127.0.0.1";
+            Console.Write("Введите свое имя:");
+            string userName = Console.ReadLine();
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("Здравствуйте, вы успешно подключились к серверу. меня зовут Абоба. Я - Бот. Список доступных команд - Помощь");
+                while (true)
+                {
+                    Console.Write(userName + ": ");
+                    // ввод сообщения
+                    string message = Console.ReadLine();
+                    message = String.Format("{0}: {1}", userName, message);
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+
+                    // получаем ответ
+                    data = new byte[256]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    message = builder.ToString();
+                    Console.WriteLine(message);
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
         }
     }
 }
