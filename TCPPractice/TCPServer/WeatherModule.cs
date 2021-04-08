bb18b2c6f1c73e48c812cdfb368ca64cf0c06378
Артур <kornet130902@gmail.com>
2021-04-08 20:31:07 +0300

    +WeatherModule

---
+++ TCPPractice/TCPServer/WeatherModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPServer
+{
+    public class WeatherResponse
+    {
+        public TemperatureInfo Main { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TemperatureInfo
+    {
+        public float Temp { get; set; }
+    }
+}
