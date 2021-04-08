d24a79897ab9fd6208229bce1b161832907e1ceb
Ирина Косых <lana-ms-lana@mail.ru>
2021-04-08 20:24:38 +0300

    сконструировала Programm.cs для сервера

--- TCPPractice/TCPServer/Program.cs
+++ TCPPractice/TCPServer/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace TCPServer
 {
     class Program
     {
+        const int port = 8888;
+        static TcpListener listener;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(client);
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                    Console.WriteLine("Пользователь подключился...");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
         }
     }
-}
